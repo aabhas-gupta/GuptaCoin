80f38df85aee8a341d6967591b2369fd8e766486
Aabhas Gupta <aabhasgupta97@gmail.com>
2021-08-01 13:10:07 +0530

    Added proof of work using nonce

--- main.js
+++ main.js
@@ -6,16 +6,26 @@ class Block{
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty+1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("Block mined: " + this.hash);
     }
 }
 
 class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
     }
 
     createGenesisBlock(){
@@ -28,7 +38,7 @@ class Blockchain{
 
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -50,8 +60,9 @@ class Blockchain{
 }
 
 let guptaCoin = new Blockchain();
-guptaCoin.addBlock(new Block(1, "02/01/2021", { amount : 4 }));
-guptaCoin.addBlock(new Block(2, "03/01/2021", { amount : 10 }));
 
-console.log("Is blockchain valid? " + guptaCoin.isChainValid());
-console.log(JSON.stringify(guptaCoin, null, 4));
+console.log("Mining block 1...");
+guptaCoin.addBlock(new Block(1, "02/01/2021", { amount : 4 }));
+
+console.log("Mining block 2...");
+guptaCoin.addBlock(new Block(2, "03/01/2021", { amount : 10 }));
