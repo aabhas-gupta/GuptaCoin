607fe8b20fdae998445f55fedd2da506ed531dd9
Aabhas Gupta <aabhasgupta97@gmail.com>
2021-08-01 14:15:51 +0530

    Added logic to sign transactions with elliptic key-pair

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 const {Blockchain, Transaction} = require('./blockchain')
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
+const myKey = ec.keyFromPrivate("036c14f1d20d59e6819971db6669e695e317cf76d3f854b8785a97ed26f17c12");
+const myWalletAddress = myKey.getPublic('hex');
 
 let guptaCoin = new Blockchain();
-guptaCoin.createTransaction(new Transaction('address1', 'address2', 100));
-guptaCoin.createTransaction(new Transaction('address2', 'address1', 50));
+
+const tx1 = new Transaction(myWalletAddress, "test1", 10);
+tx1.signTransaction(myKey);
+guptaCoin.addTransaction(tx1);
 
 console.log("\n Starting the miner...");
-guptaCoin.minePendingTransactions('aabhas-address');
+guptaCoin.minePendingTransactions(myWalletAddress);
 
-console.log("\n Balance of aabhas is ", guptaCoin.getBalanceOfAddress('aabhas-address')); 
-
-console.log("\n Starting the miner again...");
-guptaCoin.minePendingTransactions('aabhas-address');
-
-console.log("\n Balance of aabhas is ", guptaCoin.getBalanceOfAddress('aabhas-address')); 
+console.log("\n Balance of aabhas is ", guptaCoin.getBalanceOfAddress(myWalletAddress)); 
