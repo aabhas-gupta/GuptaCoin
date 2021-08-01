2b4c74ecb572cdde5c13401d082c38895abd7105
Aabhas Gupta <aabhasgupta97@gmail.com>
2021-08-01 13:41:36 +0530

    Refactored the blockchain classes

--- src/blockchain.js
+++ src/blockchain.js
@@ -97,16 +97,5 @@ class Blockchain{
     }
 }
 
-let guptaCoin = new Blockchain();
-guptaCoin.createTransaction(new Transaction('address1', 'address2', 100));
-guptaCoin.createTransaction(new Transaction('address2', 'address1', 50));
-
-console.log("\n Starting the miner...");
-guptaCoin.minePendingTransactions('aabhas-address');
-
-console.log("\n Balance of aabhas is ", guptaCoin.getBalanceOfAddress('aabhas-address')); 
-
-console.log("\n Starting the miner again...");
-guptaCoin.minePendingTransactions('aabhas-address');
-
-console.log("\n Balance of aabhas is ", guptaCoin.getBalanceOfAddress('aabhas-address')); 
+module.exports.Blockchain = Blockchain;
+module.exports.Transaction = Transaction;
